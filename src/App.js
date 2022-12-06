07447089ca4b954edc5bb6f59713c1a4c6f714fd
vernusotku <aleksandr.popenov300@yandex.ru>
2022-12-06 20:12:09 +0300

    Update version react-scripts + Add files bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Table_new from './component/table'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './bootstrap/css/bootstrap.min.css'
 import 'react-bootstrap'
 function App() {
   return (
